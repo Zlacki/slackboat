14be420aefdd844bb5db056f180e0c45c52802fd
Alex Petrovich <alex@libslack.so>
2013-09-25 04:26:30 -0500

    Basic callback functions implemented, nickserv identification, builtin function for kicking at the moment(modules come next, separating shit like that)

--- src/main.c
+++ src/main.c
@@ -7,21 +7,22 @@
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
+
 #include "util.h"
 
-int main(void) {
-	int socket_fd;
-	bool debug = true;
+int socket_fd;
+bool debug = true;
 
+int main(void) {
 	struct hostent *hp = gethostbyname(SERVER);
-	if(!slack_connect(inet_ntoa(*(struct in_addr*) (hp->h_addr_list[0])), 6667, &socket_fd)) {
+	if(!slack_connect(inet_ntoa(*(struct in_addr*) (hp->h_addr_list[0])), 6667)) {
 		printf("Failed to connect to %s.\n", SERVER);
 		return 1;
 	}
 
 	for(;;) {
 		char in_buffer[BUFFER_SIZE];
-		int i = slack_read(socket_fd, in_buffer);
+		int i = slack_read(in_buffer);
 		if(i > 0) {
 			if(debug)
 				printf("IN: %s", in_buffer);
@@ -29,19 +30,21 @@ int main(void) {
 			char sender[64], command[32], argument[32], content[256];
 			sscanf(in_buffer, ":%63s %31s %31s :%255[^\n]", sender, command, argument, content);
 
-			if(!strcmp(command, "NOTICE") && strstr(content, "Looking up your hostname") != NULL) {
-				slack_send(socket_fd, "NICK slackboat\r\n", debug);
-				slack_send(socket_fd, "USER slackboat 8 * :Slack the Boat\r\n", debug);
-			}
+			if(!strncmp(command, "NOTICE", 6) || !strncmp(command, "001", 3))
+				irc_notice_event(sender, argument, content);
 
-			if((!strcmp(command, "NOTICE") || !strcmp(command, "001")) && strstr(content, "Welcome") != NULL)
-				slack_send(socket_fd, "JOIN #pharmaceuticals\r\n", debug);
+			if(!strncmp(command, "001", 3) && strstr(content, "Welcome") != NULL)
+				irc_welcome_event();
+
+			if(!strncmp(command, "PRIVMSG", 7))
+				irc_privmsg_event(sender, argument, content);
 
 			if(!strncmp(in_buffer, "PING :", 6)) {
 				char out[BUFFER_SIZE];
+				memset(out, 0, BUFFER_SIZE);
 				char *pos = strstr(in_buffer, " ") + 1;
 				snprintf(out, 8 + strlen(pos), "PONG %s\r\n", pos);
-				slack_send(socket_fd, out, debug);
+				slack_send(out);
 			}
 		} else if(i < 0)
 			perror("Unexpected error while reading from IRC socket");
@@ -51,31 +54,31 @@ int main(void) {
 	return 0;
 }
 
-bool slack_connect(char *server, unsigned int port, int *socket_fd) {
+bool slack_connect(char *server, unsigned int port) {
 	struct sockaddr_in servaddr;
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(port);
 
-	if ( (*socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		return false;
 
 	if (inet_pton(AF_INET, server, &servaddr.sin_addr) <= 0)
 		return false;
 
-	if (connect(*socket_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0 )
+	if (connect(socket_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0 )
 		return false;
 
 	return true;
 }
 
-int slack_send(int socket_fd, char *out, bool debug) {
+int slack_send(char *out) {
     if (debug)
         printf("OUT: %s", out);
     return send(socket_fd, out, strlen(out), 0);
 }
 
-int slack_read(int socket_fd, char *in_buffer) {
+int slack_read(char *in_buffer) {
 	ssize_t nread = 0;
 	size_t tread = 0;
 	char c;
@@ -115,18 +118,52 @@ int slack_read(int socket_fd, char *in_buffer) {
 	return tread;
 }
 
-void irc_notice_event(char *sender, char *command, char *argument, char *content) {
-
+void irc_welcome_event(void) {
+	irc_join_channel("#pharmaceuticals");
 }
 
-void irc_privmsg_event(char *sender, char *command, char *argument, char *content) {
+void irc_notice_event(char *sender, char *argument, char *content) {
+	/* TODO: Load this and more from a cache file of sorts, hosts, channels, quotes, etc. */
+	if(strstr(content, "*** Looking up your hostname") != NULL) {
+		slack_send("NICK slackboat\r\n");
+		slack_send("USER slackboat 8 * :Slack the Boat\r\n");
+	}
+	if(!strncmp(sender, "NickServ", 8) && strstr(content, "please choose a different nick") != NULL) {
+		char out[256];
+		memset(out, 0, 256);
+		snprintf(out, 12 + strlen(PASSWORD), "IDENTIFY %s\r\n", PASSWORD);
+		irc_privmsg("NickServ", out);
+	}
+}
 
+void irc_privmsg_event(char *sender, char *argument, char *content) {
+	if(!strncmp(content, ".", 1) && !strncmp(sender, "sasha", 5)) {
+		char command[128], args[256];
+		sscanf(content, ".%127s %255[^\n]", command, args);
+		if(!strncmp(command, "kick", 4) || !strncmp(command, "k", 1)) {
+			char *s = strtok(args, "\0");
+			char out[256];
+			memset(out, 0, 256);
+			snprintf(out, 10 + strlen(argument) + strlen(s), "KICK %s %s\r\n", argument, s);
+			irc_privmsg("ChanServ", out);
+			memset(out, 0, 256);
+			irc_privmsg(argument, out);
+		}
+	}
 }
 
 void irc_privmsg(const char *recipient, const char *message) {
-
+	char out[256];
+	memset(out, 0, 256);
+	snprintf(out, 13 + strlen(recipient) + strlen(message), "PRIVMSG %s :%s\r\n", recipient, message);
+	slack_send(out);
+	return;
 }
 
 void irc_join_channel(const char *channel) {
-
+	char out[BUFFER_SIZE];
+	memset(out, 0, BUFFER_SIZE);
+	snprintf(out, 8 + strlen(channel), "JOIN %s\r\n", channel);
+	slack_send(out);
+	return;
 }
